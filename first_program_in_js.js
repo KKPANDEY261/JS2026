a3c90149b59ed03dc0468f1668754fce24719f9e
kkpandey261 <karanpandey261@gmail.com>
2026-01-11 13:25:45 +0530

    js first practicals

---
+++ first_program_in_js.js
@@ -0,0 +1,9 @@
+
+a="karan kumar pandey";
+b=5;
+c=10;
+console.log("this is a"+a);
+console.log(b+c);
+b=10;
+c=20;
+console.log(c*b);
---
+++ hello.js
@@ -0,0 +1,9 @@
+
+        
+b=5;
+c="10";
+d=b+c;
+
+console.log(c/b);
+console.log(c*b);
+
